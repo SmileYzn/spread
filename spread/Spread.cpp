302fedca9e6a4c17d7692d08210a6a692912ba77
SmileY <smileyznl@gmail.com>
2025-01-31 13:57:00 -0300

    Check for Snupers zoom

--- spread/Spread.cpp
+++ spread/Spread.cpp
@@ -11,25 +11,35 @@ void CSpread::ServerActivate()
     this->m_Active = g_engfuncs.pfnCVarGetPointer(tActive.name);
 }
 
-float CSpread::GetSpread(CBasePlayer *Player, float vecSpread)
+float CSpread::GetSpread(CBaseEntity *pEntity, Vector &vecSrc, Vector &vecDirShooting, float vecSpread, float flDistance, int iPenetration, int iBulletType, int iDamage, float flRangeModifier, entvars_t *pevAttacker, bool bPistol, int shared_rand)
 {
     if (this->m_Active)
     {
         if (this->m_Active->value > 0.0f)
         {
-            if (Player->m_pActiveItem)
+            if (pEntity)
             {
-                if (Player->pev->flags & FL_ONGROUND)
+                if (pEntity->pev->flags & FL_ONGROUND)
                 {
-                    if (Player->pev->punchangle.IsZero())
+                    if (pEntity->pev->velocity.Length2D() < (pEntity->pev->maxspeed / 2.0f))
                     {
-                        if (Player->pev->velocity.Length2D() < (Player->m_pActiveItem->GetMaxSpeed() / 2.0f))
+                        if (pEntity->pev->punchangle.IsZero())
                         {
-                            vecSpread = 0.0f;
+                            if (pEntity->pev->fov == DEFAULT_FOV)
+                            {
+                                if (bPistol)
+                                {
+                                    if (flDistance > 4096.0f)
+                                    {
+                                        return vecSpread;
+                                    }
+                                }
+                            }
+
+                            return 0.0f;
                         }
                     }
                 }
-            }
         }
     }
 
--- spread/Spread.h
+++ spread/Spread.h
@@ -4,7 +4,8 @@ class CSpread
 {
 public:
     void ServerActivate();
-    float GetSpread(CBasePlayer *Player, float vecSpread);
+    float GetSpread(CBaseEntity *pEntity, Vector &vecSrc, Vector &vecDirShooting, float vecSpread, float flDistance, int iPenetration, int iBulletType, int iDamage, float flRangeModifier, entvars_t *pevAttacker, bool bPistol, int shared_rand);
+
     cvar_t* m_Active = nullptr;
 };
 
